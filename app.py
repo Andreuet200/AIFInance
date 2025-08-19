77319d7d1231041fc1308e0f2ca168c648403840
Andreu Sarreta Reyes <A01285537@tec.mx>
2025-08-19 03:26:53 +0000

    2

--- app.py
+++ app.py
@@ -10,6 +10,19 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+# Definim una ruta per la pàgina principal
+@app.route("/main", methods=["GET", "POST"])
+def main():
+    #database 
+    return render_template("main.html")
+
+# Definim una ruta per la pàgina principal
+@app.route("/dbs", methods=["GET", "POST"])
+def dbs():
+    
+    return render_template("dbs.html")
+
+
 # Executem l’aplicació si el fitxer és el principal
 if __name__ == "__main__":
     app.run()
