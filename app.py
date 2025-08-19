5886931db08d1f0968accbf1cd024d4d8f8fd215
Andreu Sarreta Reyes <A01285537@tec.mx>
2025-08-19 03:47:02 +0000

    1

--- app.py
+++ app.py
@@ -1,28 +1,20 @@
+from flask import Flask,render_template,request
 
-# Importem les classes i funcions necessàries de Flask
-from flask import Flask, render_template, request
-
-# Creem una instància de l'aplicació Flask
 app = Flask(__name__)
 
-# Definim una ruta per la pàgina principal
-@app.route("/", methods=["GET", "POST"])
+@app.route("/",methods=["GET","POST"])
 def index():
-    return render_template("index.html")
+    return(render_template("index.html"))
 
-# Definim una ruta per la pàgina principal
-@app.route("/main", methods=["GET", "POST"])
+@app.route("/main",methods=["GET","POST"])
 def main():
-    #database 
-    return render_template("main.html")
+    #database
+    return(render_template("main.html"))
 
-# Definim una ruta per la pàgina principal
-@app.route("/dbs", methods=["GET", "POST"])
+@app.route("/dbs",methods=["GET","POST"])
 def dbs():
-    
-    return render_template("dbs.html")
+    q = float(request.form.get("q"))
+    return(render_template("dbs.html", r=(-50.6*q)+90.2))
 
-
-# Executem l’aplicació si el fitxer és el principal
 if __name__ == "__main__":
     app.run()
